5339e2826353bd6af5cbf540bba8c3badc5c3b5d
hashtag-united <aadithya.naresh5@gmail.com>
2020-07-21 00:03:32 +0530

    Formatted List Item

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom'
+import moment from 'moment'
 
 export const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <h3><NavLink to={`/edit/${id}`}>{description}</NavLink></h3>
-    <p>{amount} - {createdAt}</p>
+    <p>
+    {'₹'+amount.toLocaleString('en-IN')} 
+    - 
+    {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
   
   </div>
 );
